61f0e4e25af14cf1c57bb98b8e27e41f13616a2a
Foxd5 <38409373+Foxd5@users.noreply.github.com>
2024-08-30 00:10:39 -0400

    Ship Explosion Sounds
    Fixed ship explosion sounds so enemies probably sound like theyre exploding on death

--- Assets/Scripts/EnemyHealthManager.cs
+++ Assets/Scripts/EnemyHealthManager.cs
@@ -4,16 +4,25 @@ using UnityEngine;
 
 public class EnemyHealthManager : MonoBehaviour
 {
+
+    public AudioClip explodeSound;
     public float healthAmount = 100f;
     public GameObject enemyexplodePrefab;
+    private AudioSource explodesoundSource;
+
+    private void Start()
+    {
+        //need to get audio component so i can change volume later
+        explodesoundSource = GetComponent<AudioSource>();
+    }
+
     public void TakeDamage(float damage)
     {
         healthAmount -= damage;
-        Debug.Log("Enemy Health: " + healthAmount);
+        //Debug.Log("Enemy Health: " + healthAmount);
 
         if(healthAmount <= 0)
         {
-
             Die();
         }
 
@@ -22,7 +31,22 @@ public class EnemyHealthManager : MonoBehaviour
     void Die()
     {
         Instantiate(enemyexplodePrefab, transform.position, Quaternion.identity);
+        
+        //create a temporary GameObject to play the sound
+        //needed to do this because destroy() was preventing
+        //death sound from being made. Then, because its a new
+        //audio source, I had to make a new one to control the volume
+        //because the new audio source doesnt access the audio components volume control
+        GameObject tempAudio = new GameObject("TempAudio");
+        AudioSource audioSource = tempAudio.AddComponent<AudioSource>();
+        audioSource.clip = explodeSound;
+        audioSource.volume = explodesoundSource.volume; //had to add this because
+        audioSource.Play();
+
+        //destroy the temp GameObject after the sound has played
+        Destroy(tempAudio, explodeSound.length);
         Destroy(gameObject);
+
     }
     
 }
