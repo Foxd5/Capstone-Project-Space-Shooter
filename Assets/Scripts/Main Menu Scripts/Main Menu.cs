75f2dbe608aff1d287e326849abb873280f121df
Foxd5 <38409373+Foxd5@users.noreply.github.com>
2024-08-30 15:13:17 -0400

    Moving stuff on canvas

--- Assets/Scripts/Main Menu Scripts/Main Menu.cs
+++ Assets/Scripts/Main Menu Scripts/Main Menu.cs
@@ -5,9 +5,21 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject controlPanel;
+
     public void StartGame()
     {
         SceneManager.LoadScene("MainScene"); 
+    }
+
+    public void ShowControls()
+    {
+        controlPanel.SetActive(true);
+    }
+
+    public void HideControls()
+    {
+        controlPanel.SetActive(false);
     }
 
     public void QuitGame()
